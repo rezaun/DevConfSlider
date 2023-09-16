2e279b40dcdd370cfe48b6607ed9f7b455cf1105
Rezaun Kabir <rezaun.kabir@gmail.com>
2023-09-16 15:46:22 +0600

    initial commit

---
+++ devconf-slider.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Dev Conf Slider
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This is a slider plugin
+Author: Rezaun Kabir
+Version: 1.0
+Author URI: https://rezaun.netlify.app/
+*/
